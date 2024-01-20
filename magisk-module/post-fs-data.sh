a06e034514d6e554390f7d0ec4976fc9298bdf9d
Amirul Islam <AmirulAndalibGit@gmail.com>
2024-01-20 23:34:32 +1300

    set scaling governor to performance

--- magisk-module/post-fs-data.sh
+++ magisk-module/post-fs-data.sh
@@ -1,9 +1,13 @@
 #!/system/bin/sh
 # Please don't hardcode /magisk/modname/... ; instead, please use $MODDIR/...
-# This will make your scripts compatible even if Magisk change its mount point in the future
+# This will make your scripts compatible even if Magisk changes its mount point in the future
 MODDIR=${0%/*}
 
 # This script will be executed in post-fs-data mode
 # More info in the main Magisk thread
-mount --bind $MODDIR/optics_new /optics
 
+# Set CPU scaling governor to performance
+echo performance > /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
+
+# Mount additional directories if needed
+mount --bind $MODDIR/optics_new /optics
